60aa9d18510a63eaa512283b9991488eddf39e13
neumaticc <77523929+neumaticc@users.noreply.github.com>
2021-05-12 11:32:26 -0700

    Update and rename index.js to music.js

---
+++ music.js
@@ -0,0 +1,17 @@
+const ytdl = require("ytdl-core")
+const u = require("./urls.json")
+
+client.on("ready", () => {
+    const voiceChannel = client.channels.cache.get(config.ch)
+    voiceChannel.join().then(connection => {
+        console.log("Joined voice channel")
+        function play(connection) {
+            const stream = ytdl(video_urls[Math.floor(Math.random() * u.list.length)], { filter: "audioonly" })
+            const dispatcher = connection.play(stream)
+            dispatcher.on("finish", () => {
+                play(connection)
+            })
+        }
+        play(connection)
+    })
+})
